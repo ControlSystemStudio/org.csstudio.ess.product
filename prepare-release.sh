a90a478ce14bf37535b9f190698d80fdf3ea9444
Claudio Rosati <claudio.rosati@esss.se>
2016-08-26 17:26:33 +0200

    Fixing bad version numbers for release

--- prepare-release.sh
+++ prepare-release.sh
@@ -2,17 +2,17 @@
 set -e
 
 # Check parameters
-VERSION=$1
-COMPATLINK=$2
-MILESTONE=$3
-NOTES=$4
-PUSH=$5
-BUILD_DIR="../org.csstudio.product/build"
-if [ $# != 5 ]
-then 
-  echo You must provide the product version, compat link, milestone, notes \(e.g. \"prepare_release.sh 3.3.0 \"https://github\" \"https://github\" \"Some notes\"\"\)
-exit -1
+if [ $# != 4 ]
+then
+  echo You must provide the product version, milestone, notes, git \(e.g. \"prepare_release.sh 3.3.0 \"https://github\" \"https://github\" \"Some notes\" false\"\)
+  exit -1
 fi
+
+VERSION=$1
+MILESTONE=$2
+NOTES=$3
+PUSH=$4
+BUILD_DIR="../org.csstudio.product/build"
 
 echo ::: Prepare splash :::
 java -jar $BUILD_DIR/ImageLabeler-2.0.jar $VERSION 462 43 ../org.csstudio.product/plugins/org.csstudio.product/splash-template.bmp plugins/se.ess.ics.csstudio.product/splash.bmp "European Spallation Source Edition" 19 151 plugins/se.ess.ics.csstudio.product/icons/css96.png 366 140
@@ -22,15 +22,16 @@ echo 0=$VERSION > plugins/se.ess.ics.csstudio.product/about.mappings
 echo ::: Updating plugin versions :::
 mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:0.20.0:set-version -DnewVersion=$VERSION -Dartifacts=se.ess.ics.csstudio,se.ess.ics.csstudio.features,org.csstudio.ess.product.configuration.feature,org.csstudio.ess.product.core.feature,org.csstudio.ess.product.eclipse.feature,se.ess.ics.csstudio.platform-plugins,org.csstudio.jre6.fragment,se.ess.ics.csstudio.plugins,se.ess.ics.csstudio.product,se.ess.ics.csstudio.startup.intro,se.ess.ics.csstudio.repository
 # update product because set-version doesn't
-sed -i 's/\(\<product[^>]\+\? version=\"\)[^"]*\("[^>]\+\?>\)/\1'${VERSION}'\2/g'  repository/cs-studio.product
+sed -i 's/\(\<product[^>]\+\? version=\"\)[^"]*\("[^>]\+\?>\)/\1'${VERSION}'\2/g' repository/cs-studio-ess.product
 
-HTML="<h2>Version ${VERSION} - $(date +"%Y-%m-%d")</h2>
-<ul>
-<li>See specific application changelogs</li>
-<li>${NOTES}</li>
-<li><a href=\"${COMPATLINK}\" shape=\"rect\">Compatibility Notes and Know Bugs</a></li>
-<li><a href=\"${MILESTONE}\" shape=\"rect\">Closed Issues</a></li>
-</ul>"
+HTML="<h2>Version ${VERSION} - $(date +"%Y-%m-%d")</h2><ul>"
+
+if [ -n ${NOTES} ];
+then
+  HTML="${HTML}<li>${NOTES}</li>";
+fi
+
+HTML="${HTML}<li><a href=\"${MILESTONE}\" shape=\"rect\">Closed Issues</a></li></ul>"
 
 # html encode &
 HTML=$(echo $HTML | sed 's/&/\&amp;/g;')
@@ -51,15 +52,15 @@ HTML="${HTML//\$/\\\$}"
 # remove newlines
 HTML="${HTML//[$'\n']/}"
 
-sed -i '/<\/p>/ a\ \n'"${HTML}" plugins/org.csstudio.startup.intro/html/changelog.html
+sed -i ".bak" '/<\/p>/ a\ \n'"${HTML}" plugins/se.ess.ics.csstudio.startup.intro/html/changelog.html
 
-echo ::: Committing and tagging version $VERSION :::
-git commit -a -m "Updating changelog, splash, manifests to version $VERSION"
 if [ "$PUSH" = "true" ]
 then
+  echo ::: Committing version $VERSION :::
+  git commit -a -m "Updating changelog, splash, manifests to version $VERSION"
   echo ::: Tagging version $VERSION :::
-  git tag CSS-$VERSION
+  git tag ESS-CSS-$VERSION
   echo ::: Pushing changes :::
   git push origin
-  git push origin CSS-$VERSION
+  git push origin ESS-CSS-$VERSION
 fi
