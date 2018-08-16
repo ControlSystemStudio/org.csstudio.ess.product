3cf8d70b805925b520b41d7094043b9cb3ab7dde
Claudio Rosati <Claudio.Rosati@esss.se>
2018-08-16 09:07:55 +0200

    Another change to have the version numbering working.

---
+++ prepare-next-jenkins-release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+
+export CSSVER=$(cat features/org.csstudio.ess.product.configuration.feature/rootfiles/ess-version.txt)
+export GITREPO=$1
+export JBUILD=$2
+
+if [ "$GITREPO" = "master" ]
+then
+
+	echo "----------------------------------------------------------------------"
+	echo "Adding build number ${JBUILD} to version ${CSSVER}..."
+	echo "----------------------------------------------------------------------"
+
+	source prepare-next-release.sh ${CSSVER}b${JBUILD} false
+
+	echo "----------------------------------------------------------------------"
+	echo "Version number ${CSSVER}b${JBUILD} succesfully updated."
+	echo "----------------------------------------------------------------------"
+
+fi
