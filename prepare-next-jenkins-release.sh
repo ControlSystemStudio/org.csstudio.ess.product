dc0d0608e2b6144b2baf8da26184b574d21c9349
Claudio Rosati <Claudio.Rosati@esss.se>
2018-08-16 15:21:57 +0200

    Changes focused on "sed" not working on Jenkins.

--- prepare-next-jenkins-release.sh
+++ prepare-next-jenkins-release.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #
 
-PROJECT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-CSSVER=$(cat ${PROJECT_PATH}/features/org.csstudio.ess.product.configuration.feature/rootfiles/ess-version.txt)
+CSSVER=$(cat features/org.csstudio.ess.product.configuration.feature/rootfiles/ess-version.txt)
 GITREPO=$1
 JBUILD=$2
 
@@ -14,7 +12,7 @@ then
 	echo "Adding build number ${JBUILD} to version ${CSSVER}..."
 	echo "----------------------------------------------------------------------"
 
-	${PROJECT_PATH}/prepare-next-release.sh ${CSSVER}b${JBUILD} false
+	./prepare-next-release.sh ${CSSVER}b${JBUILD} false
 
 	if [ $? -eq 0 ]
 	then
--- prepare-next-release.sh
+++ prepare-next-release.sh
@@ -11,7 +11,7 @@ fi
 VERSION=$1
 PUSH=$2
 PROJECT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BUILD_DIR="${PROJECT_PATH}/build"
+BUILD_DIR="./build"
 
 echo ""
 echo "===="
@@ -20,32 +20,34 @@ echo "====  JDK used: " $JAVA_HOME
 echo "===="
 
 echo ::: Prepare splash :::
-java -jar $BUILD_DIR/ImageLabeler-2.0.jar "$VERSION (development)" 462 43 ${BUILD_DIR}/splash-template.bmp ${PROJECT_PATH}/plugins/se.ess.ics.csstudio.product/splash.bmp "European Spallation Source Edition" 19 151 ${PROJECT_PATH}/plugins/se.ess.ics.csstudio.startup.intro/icons/ess96.png 366 140
+java -jar $BUILD_DIR/ImageLabeler-2.0.jar "$VERSION (development)" 462 43 ${BUILD_DIR}/splash-template.bmp plugins/se.ess.ics.csstudio.product/splash.bmp "European Spallation Source Edition" 19 151 plugins/se.ess.ics.csstudio.startup.intro/icons/ess96.png 366 140
 
 echo ::: Change about dialog version :::
-echo 0=$VERSION > ${PROJECT_PATH}/plugins/se.ess.ics.csstudio.product/about.mappings
+echo 0=$VERSION > plugins/se.ess.ics.csstudio.product/about.mappings
 
 echo ::: Change Ansible reference file :::
-echo $VERSION > ${PROJECT_PATH}/features/org.csstudio.ess.product.configuration.feature/rootfiles/ess-version.txt
+echo $VERSION > features/org.csstudio.ess.product.configuration.feature/rootfiles/ess-version.txt
 
 echo ::: Updating plugin versions :::
 mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:1.0.0:set-version -DnewVersion=$VERSION -Dartifacts=se.ess.ics.csstudio,se.ess.ics.csstudio.features,org.csstudio.ess.product.configuration.feature,org.csstudio.ess.product.core.feature,org.csstudio.ess.product.eclipse.feature,se.ess.ics.csstudio.plugins,se.ess.ics.csstudio.display.builder,se.ess.ics.csstudio.fonts,se.ess.ics.csstudio.product,se.ess.ics.csstudio.startup.intro,se.ess.ics.csstudio.repository
 
 # update product because set-version doesn't
 echo ::: Updating product versions in product files :::
-echo ::::: ${PROJECT_PATH}/repository/alarm-config.product
-sed -i '' -e 's/\(<product[^>]* version="\)[^"]*\("[^>]*>\)/\1'${VERSION}'\2/g' "${PROJECT_PATH}/repository/alarm-config.product"
-echo ::::: ${PROJECT_PATH}/repository/alarm-notifier.product
-sed -i '' -e 's/\(<product[^>]* version="\)[^"]*\("[^>]*>\)/\1'${VERSION}'\2/g' "${PROJECT_PATH}/repository/alarm-notifier.product"
-echo ::::: ${PROJECT_PATH}/repository/alarm-server.product
-sed -i '' -e 's/\(<product[^>]* version="\)[^"]*\("[^>]*>\)/\1'${VERSION}'\2/g' "${PROJECT_PATH}/repository/alarm-server.product"
-echo ::::: ${PROJECT_PATH}/repository/cs-studio-ess.product
-sed -i '' -e 's/\(<product[^>]* version="\)[^"]*\("[^>]*>\)/\1'${VERSION}'\2/g' "${PROJECT_PATH}/repository/cs-studio-ess.product"
-echo ::::: ${PROJECT_PATH}/repository/jms2rdb.product
-sed -i '' -e 's/\(<product[^>]* version="\)[^"]*\("[^>]*>\)/\1'${VERSION}'\2/g' "${PROJECT_PATH}/repository/jms2rdb.product"
+COMMAND='s/\(<product[^>]* version="\)[^"]*\("[^>]*>\)/\1'${VERSION}'\2/g'
+echo ::::: repository/alarm-config.product
+sed -i '' -e "${COMMAND}" repository/alarm-config.product
+echo ::::: repository/alarm-notifier.product
+sed -i '' -e "${COMMAND}" repository/alarm-notifier.product
+echo ::::: repository/alarm-server.product
+sed -i '' -e "${COMMAND}" repository/alarm-server.product
+echo ::::: repository/cs-studio-ess.product
+sed -i '' -e "${COMMAND}" repository/cs-studio-ess.product
+echo ::::: repository/jms2rdb.product
+sed -i '' -e "${COMMAND}" repository/jms2rdb.product
 
 echo ::: Updating product versions in master POM file :::
-sed -i '' -e 's/\(<product\.version>\)[^<]*\(\<\/product\.version>\)/\1'${VERSION}'\2/g' "${PROJECT_PATH}/pom.xml"
+COMMAND='s/\(<product\.version>\)[^<]*\(\<\/product\.version>\)/\1'${VERSION}'\2/g'
+sed -i '' -e "${COMMAND}" pom.xml
 
 if [ "$PUSH" = "true" ]
 then
