e7a9fa2f538a1386fae23bd1dad0eb24e2afe07a
Claudio Rosati <Claudio.Rosati@esss.se>
2019-03-11 16:30:56 +0100

    Ready for a new deployment.

--- merge.sh
+++ merge.sh
@@ -8,14 +8,17 @@ declare -a repos=("org.csstudio.display.builder"
                 "cs-studio"
                 "maven-osgi-bundles"
                 "org.csstudio.ess.product"
-               )
+                 )
+
+git commit -a -m "Updating changelog, splash, manifests to version $VERSION"
+git push origin
 
 for i in "${repos[@]}"
 do
     cd ../$i/
     git checkout production
     git pull origin production
-    git merge master
+    git merge master -m "Merge master into production."
     git push origin production
     git tag ESS-CS-Studio-$VERSION
     git checkout master
