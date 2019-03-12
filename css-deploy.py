5ed1cebe7ed89b6c89d4903677afddce53d13105
jkazan <johannes.k@live.com>
2019-03-12 19:45:44 +0100

    updated version (missed this last commit)

--- css-deploy.py
+++ css-deploy.py
@@ -12,7 +12,7 @@
 #  You should have received a copy of the GNU General Public License along with
 #  this program. If not, see https://www.gnu.org/licenses/gpl.txt
 
-__version__ = '0.1.7'
+__version__ = '0.2.0'
 __author__ = 'Johannes C. Kazantzidis'
 
 import argparse
