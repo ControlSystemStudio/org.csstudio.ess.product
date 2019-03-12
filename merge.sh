26dd530bae880a54228b570a9a52916938fe8345
jkazan <johannes.k@live.com>
2019-03-12 19:57:42 +0100

    Updating changelog, splash, manifests to version 4.6.1.13

--- merge.sh
+++ merge.sh
@@ -3,21 +3,22 @@
 VERSION=$1
 
 declare -a repos=(
-    "org.csstudio.display.builder"
-    "cs-studio-thirdparty"
-    "ess-css-extra"
-    "cs-studio"
-    "maven-osgi-bundles"
+    # "org.csstudio.display.builder"
+    # "cs-studio-thirdparty"
+    # "ess-css-extra"
+    # "cs-studio"
+    # "maven-osgi-bundles"
     "org.csstudio.ess.product"
 )
 
 declare -a gitcmds=(
-    "git checkout production"
-    "git pull origin production"
-    'git merge master -m "Merge master into production."'
-    "git push origin production"
-    "git tag ESS-CS-Studio-$VERSION"
-    "git checkout master"
+    # "git checkout production"
+    # "git pull origin production"
+    # 'git merge master -m "Merge master into production."'
+    # "git push origin production"
+    # "git tag ESS-CS-Studio-$VERSION"
+    # "git checkout master"
+    "git asdlkj"
 )
 
 git commit -a -m "Updating changelog, splash, manifests to version $VERSION"
