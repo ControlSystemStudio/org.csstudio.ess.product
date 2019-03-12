e82a45e1481805ff3f7fc5ddcd9ec63653a84250
jkazan <johannes.k@live.com>
2019-03-12 19:45:20 +0100

    aborting merge script if error occurs

--- merge.sh
+++ merge.sh
@@ -2,26 +2,34 @@
 
 VERSION=$1
 
-declare -a repos=("org.csstudio.display.builder"
-                "cs-studio-thirdparty"
-                "ess-css-extra"
-                "cs-studio"
-                "maven-osgi-bundles"
-                "org.csstudio.ess.product"
-                 )
+declare -a repos=(
+    "org.csstudio.display.builder"
+    "cs-studio-thirdparty"
+    "ess-css-extra"
+    "cs-studio"
+    "maven-osgi-bundles"
+    "org.csstudio.ess.product"
+)
+
+declare -a gitcmds=(
+    "git checkout production"
+    "git pull origin production"
+    'git merge master -m "Merge master into production."'
+    "git push origin production"
+    "git tag ESS-CS-Studio-$VERSION"
+    "git checkout master"
+)
 
 git commit -a -m "Updating changelog, splash, manifests to version $VERSION"
 git push origin
 
-for i in "${repos[@]}"
-do
+for i in "${repos[@]}"; do
     cd ../$i/
-    git checkout production
-    git pull origin production
-    git merge master -m "Merge master into production."
-    git push origin production
-    git tag ESS-CS-Studio-$VERSION
-    git checkout master
+    for k in "${gitcmds[@]}"; do
+        $k
+        if [[ $? != 0 ]]; then
+            echo "An error occured when running $k"
+            exit 1
+        fi
+    done
 done
-
-exit 0
