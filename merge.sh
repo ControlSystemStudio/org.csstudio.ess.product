ce15272c3f157e9d0943984c74a02f9c3d84f68c
Claudio Rosati <Claudio.Rosati@esss.se>
2019-03-18 11:10:52 +0100

    Merge script updated

--- merge.sh
+++ merge.sh
@@ -2,18 +2,18 @@
 
 VERSION=$1
 
-# "org.csstudio.display.builder"
 declare -a repos=(
-    "cs-studio-thirdparty"
     "ess-css-extra"
-    "cs-studio"
     "maven-osgi-bundles"
+    "cs-studio-thirdparty"
+    "cs-studio"
+    "org.csstudio.display.builder"
     "org.csstudio.ess.product"
 )
 
-# "git pull origin production"
 declare -a gitcmds=(
     "git checkout production"
+    "git pull origin production"
     "git merge master -m \"Merge master into production.\""
     "git push origin production"
     "git tag ESS-CS-Studio-$VERSION"
