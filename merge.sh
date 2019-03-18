c4cd21932b36963ba562f20972c2f353aed10eb1
Claudio Rosati <Claudio.Rosati@esss.se>
2019-03-18 10:59:59 +0100

    Updating changelog, splash, manifests to version 4.6.1.17

--- merge.sh
+++ merge.sh
@@ -14,7 +14,7 @@ declare -a repos=(
 declare -a gitcmds=(
     "git checkout production"
     "git pull origin production"
-    'git merge master -m "Merge master into production."'
+    "git merge master -m \"Merge master into production.\""
     "git push origin production"
     "git tag ESS-CS-Studio-$VERSION"
     "git checkout master"
