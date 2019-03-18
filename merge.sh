5728e8ea2d5e63113558508e498c0d0b72a10753
jkazan <johannes.k@live.com>
2019-03-18 11:40:21 +0100

    merge bug fix

--- merge.sh
+++ merge.sh
@@ -3,18 +3,18 @@
 VERSION=$1
 
 declare -a repos=(
-    "org.csstudio.display.builder"
-    "cs-studio-thirdparty"
     "ess-css-extra"
-    "cs-studio"
     "maven-osgi-bundles"
+    "cs-studio-thirdparty"
+    "cs-studio"
+    "org.csstudio.display.builder"
     "org.csstudio.ess.product"
 )
 
 declare -a gitcmds=(
     "git checkout production"
     "git pull origin production"
-    'git merge master -m "Merge master into production."'
+    "git merge master -m 'Merge master into production.'"
     "git push origin production"
     "git tag ESS-CS-Studio-$VERSION"
     "git checkout master"
@@ -28,6 +28,7 @@ for i in "${repos[@]}"; do
     for k in "${gitcmds[@]}"; do
         $k
         if [[ $? != 0 ]]; then
+            echo "Error occurred running: $k"
             exit 1
         fi
     done
