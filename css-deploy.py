d8e8fc4144a8b221376340243892384361c6845d
jkazan <johannes.k@live.com>
2019-03-18 11:39:43 +0100

    cleanup

--- css-deploy.py
+++ css-deploy.py
@@ -252,7 +252,7 @@ def patReplace(path, pattern, repl):
 
     Args:
         path: Path to file in which to replace text.
-        pattern: Regular expression pattern to search fore.
+        pattern: Regular expression pattern to search for.
         repl: Replacement text with which to replace text matching to `pattern`.
     """
     pat = re.compile(pattern)
@@ -490,8 +490,8 @@ def updateConfluenceRelease(css_version, next_version, ce_version, auth):
     what_changed_stop = result.split("Production</h2>")
     result = what_changed_start[0] \
       + "What Is Changed</h4>" \
-      + '<ul><li><span style="color: rgb(0,0,0);">Nothing yet</span>.</li></ul>' \
-      +"<h2>Production</h2>" \
+      + '<ul><li><span style="color: rgb(0,0,0);">Nothing yet</span>.</li>' \
+      +"</ul><h2>Production</h2>" \
       + what_changed_stop[1]
 
     # Get url for the release
@@ -587,6 +587,20 @@ def inform(text):
 def main(css_version):
     """Main for automatic CSS deployment.
 
+    Ensure user is on master branch.
+    Check if release exists on Jira.
+    Check if JAVA_HOME env var exists.
+    Compare deploy version against artifactory and verify with user.
+    Get chengelog notes by parsing all closed jira tickets for release.
+    Deduce CSS CE version.
+    Prepare-release (Update splash, 'about' dialog and plugin versions).
+    Update changelog.
+    update pom.xml.
+    Merge all repositories into production.
+    Update confluence Release Notes page.
+    Prepare-next-release (splash, 'about' dialog and plugin, ansible reference).
+    Update confluence Release page.
+
     Args:
         css_version: CSS version to be released.
     """
@@ -599,7 +613,7 @@ def main(css_version):
     checkJavaHome()                     # Check if JAVA_HOME env var exists
     checkVersion(css_version)           # Check if user entered correct version
 
-    notes = getChangelogNotes(css_version, auth) # Get chengelog notes
+    notes = getChangelogNotes(css_version, auth) # Get changelog notes
     ce_version = getCEVersion(css_version)       # Get CSS CE version
 
     dir_path = os.path.dirname(os.path.abspath(__file__))+"/" # path to dir
